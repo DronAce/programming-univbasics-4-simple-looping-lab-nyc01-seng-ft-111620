9bf0aab9508b131507d0336a0ef64709ba6a5316
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 05:00:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,10 +22,8 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  count = 0
-  array.length.times do
+  array.length.times do |count|
     array[count] = array[count].to_s
-    count += 1
   end
   array
 end
